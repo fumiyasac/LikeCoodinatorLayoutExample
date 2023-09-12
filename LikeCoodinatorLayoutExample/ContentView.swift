047881346ecdaac77c72cf5c91edec949b26b27f
fumiyasac <just1factory@gmail.com>
2023-09-12 14:57:07 +0900

    initial commit

---
+++ LikeCoodinatorLayoutExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LikeCoodinatorLayoutExample
+//
+//  Created by 酒井文也 on 2023/09/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LikeCoodinatorLayoutExample/LikeCoodinatorLayoutExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikeCoodinatorLayoutExampleApp.swift
+//  LikeCoodinatorLayoutExample
+//
+//  Created by 酒井文也 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct LikeCoodinatorLayoutExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
