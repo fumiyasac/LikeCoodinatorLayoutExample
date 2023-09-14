5d0358c138241fb6463baf409719472f186df0f2
fumiyasac <just1factory@gmail.com>
2023-09-14 23:39:24 +0900

    要素の追加対応

---
+++ LikeCoodinatorLayoutExample/View/Components/ContentRowView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentRowView.swift
+//  LikeCoodinatorLayoutExample
+//
+//  Created by 酒井文也 on 2023/09/14.
+//
+
+import SwiftUI
+
+struct ContentRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentRowView()
+    }
+}
---
+++ LikeCoodinatorLayoutExample/View/Components/ContentSheetView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentSheetView.swift
+//  LikeCoodinatorLayoutExample
+//
+//  Created by 酒井文也 on 2023/09/14.
+//
+
+import SwiftUI
+
+struct ContentSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentSheetView()
+    }
+}
