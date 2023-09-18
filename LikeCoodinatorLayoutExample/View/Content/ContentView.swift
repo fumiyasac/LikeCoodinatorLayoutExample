59d66543cfbe1ff827656a299b78687c7b28dec5
fumiyasac <just1factory@gmail.com>
2023-09-18 09:27:19 +0900

    画面の骨格を作成する

--- LikeCoodinatorLayoutExample/View/Content/ContentView.swift
+++ LikeCoodinatorLayoutExample/View/Content/ContentView.swift
@@ -8,22 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    // MARK: - property
+
+    //
+    private let verticalOffsetThreshold = -142.0
+
+    //
+    @State private var shouldCollapse = false
+
+    // MARK: - body
+
     var body: some View {
         NavigationStack {
-            ZStack {
-            
+            //
+            ZStack(alignment: .topLeading) {
+                //
+                ZStack(alignment: .top) {
+                    //
+                }
+                //
+                ScrollViewWithVerticalOffset(
+                    //
+                    onOffsetChange: { offset in
+                        //
+                        shouldCollapse = offset < verticalOffsetThreshold
+                    },
+                    //
+                    content: {
+                        //
+                        Spacer()
+                            .frame(height: 130.0)
+                        //
+//                        LazyVStack(spacing: 0.0, pinnedViews: .sectionHeaders) {
+//                            Section(
+//                                header:
+//                                    ContentHeaderView()
+//                                )
+//                                .frame(height: shouldCollapse ? 53.0 : 82.0)
+//                                .animation(.easeInOut(duration: 0.08), value: shouldCollapse)
+//                            ) {
+//                                //
+//                                ContentRowView()
+//                            }
+//                        }
+//                        .background(.white)
+                    }
+                )
             }
         }
-        
-        
-        
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
     }
 }
 
